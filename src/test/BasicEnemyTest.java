80c8744199feed0a84978707978fea531150f874
Anbang Cheng <z5237233@ad.unsw.edu.au>
2021-07-15 20:03:41 +1000

    splite tests

--- src/test/BasicEnemyTest.java
+++ src/test/BasicEnemyTest.java
@@ -19,18 +19,55 @@ public class BasicEnemyTest {
         PathPosition Pp = new PathPosition(currentPositionInPath, orderedPath);
         BasicEnemy e = new BasicEnemy(Pp, 4, 3, 1, 2, "Jeffery");
         assertEquals(e.getAggressivity(), 4);
-        assertEquals(e.getHealth(), 3);
-        assertEquals(e.getAttackRange(), 1);
-        assertEquals(e.getSupportRange(), 2);
-        assertEquals(e.getName(), "Jeffery");
         e.setAggressivity(1);
         assertEquals(e.getAggressivity(), 1);
+    }
+
+    @Test
+    public void enemyTest2(){
+        Pair p = new Pair(1, 1);
+        List<Pair<Integer, Integer>> orderedPath;
+        orderedPath.add(p);
+        PathPosition Pp = new PathPosition(currentPositionInPath, orderedPath);
+        BasicEnemy e = new BasicEnemy(Pp, 4, 3, 1, 2, "Jeffery");
+        assertEquals(e.getHealth(), 3);
         e.setHealth(1);
-        assertEquals(e.getHealth(), 1);
+    }
+
+    @Test
+    public void enemyTest3(){
+        Pair p = new Pair(1, 1);
+        List<Pair<Integer, Integer>> orderedPath;
+        orderedPath.add(p);
+        PathPosition Pp = new PathPosition(currentPositionInPath, orderedPath);
+        BasicEnemy e = new BasicEnemy(Pp, 4, 3, 1, 2, "Jeffery");
+    
+        assertEquals(e.getAttackRange(), 1);
         e.setAttackRange(2);
         assertEquals(e.getAttackRange(), 2);
+    }
+
+    @Test
+    public void enemyTest4(){
+        Pair p = new Pair(1, 1);
+        List<Pair<Integer, Integer>> orderedPath;
+        orderedPath.add(p);
+        PathPosition Pp = new PathPosition(currentPositionInPath, orderedPath);
+        BasicEnemy e = new BasicEnemy(Pp, 4, 3, 1, 2, "Jeffery");
+        
+        assertEquals(e.getSupportRange(), 2);
         e.setSupportRange(1);
         assertEquals(e.getSupportRange(), 1);
+    }
+
+    @Test
+    public void enemyTest5(){
+        Pair p = new Pair(1, 1);
+        List<Pair<Integer, Integer>> orderedPath;
+        orderedPath.add(p);
+        PathPosition Pp = new PathPosition(currentPositionInPath, orderedPath);
+        BasicEnemy e = new BasicEnemy(Pp, 4, 3, 1, 2, "Jeffery");
+        assertEquals(e.getName(), "Jeffery");
         e.setName("Steve");
         assertEquals(e.getName(), "Steve");
     }
