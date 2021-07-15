5b07315d79e0ab844eb882afb81546b876eae6a0
Anbang Cheng <z5237233@ad.unsw.edu.au>
2021-07-15 23:35:30 +1000

    add tests for Slug, Zombie and Vampire class

--- src/test/BasicEnemyTest.java
+++ src/test/BasicEnemyTest.java
@@ -2,21 +2,25 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 import unsw.loopmania.BasicEnemy;
 import unsw.loopmania.PathPosition;
+import unsw.loopmania.Slug;
+import unsw.loopmania.Zombie;
+
 import org.javatuples.Pair;
 
 public class BasicEnemyTest {
     @Test
     public void enemyTest1(){
-        Pair p = new Pair(1, 1);
-        List<Pair<Integer, Integer>> orderedPath;
+        Pair p = new Pair<Integer,Integer>(1, 1);
+        List<Pair<Integer, Integer>> orderedPath = new LinkedList<Pair<Integer, Integer>>();
         orderedPath.add(p);
-        PathPosition Pp = new PathPosition(currentPositionInPath, orderedPath);
+        PathPosition Pp = new PathPosition(0, orderedPath);
         BasicEnemy e = new BasicEnemy(Pp, 4, 3, 1, 2, "Jeffery");
         assertEquals(e.getAggressivity(), 4);
         e.setAggressivity(1);
@@ -25,10 +29,10 @@ public class BasicEnemyTest {
 
     @Test
     public void enemyTest2(){
-        Pair p = new Pair(1, 1);
-        List<Pair<Integer, Integer>> orderedPath;
+        Pair p = new Pair<Integer,Integer>(1, 1);
+        List<Pair<Integer, Integer>> orderedPath = new LinkedList<Pair<Integer, Integer>>();
         orderedPath.add(p);
-        PathPosition Pp = new PathPosition(currentPositionInPath, orderedPath);
+        PathPosition Pp = new PathPosition(0, orderedPath);
         BasicEnemy e = new BasicEnemy(Pp, 4, 3, 1, 2, "Jeffery");
         assertEquals(e.getHealth(), 3);
         e.setHealth(1);
@@ -36,10 +40,10 @@ public class BasicEnemyTest {
 
     @Test
     public void enemyTest3(){
-        Pair p = new Pair(1, 1);
-        List<Pair<Integer, Integer>> orderedPath;
+        Pair p = new Pair<Integer,Integer>(1, 1);
+        List<Pair<Integer, Integer>> orderedPath = new LinkedList<Pair<Integer, Integer>>();
         orderedPath.add(p);
-        PathPosition Pp = new PathPosition(currentPositionInPath, orderedPath);
+        PathPosition Pp = new PathPosition(0, orderedPath);
         BasicEnemy e = new BasicEnemy(Pp, 4, 3, 1, 2, "Jeffery");
     
         assertEquals(e.getAttackRange(), 1);
@@ -49,10 +53,10 @@ public class BasicEnemyTest {
 
     @Test
     public void enemyTest4(){
-        Pair p = new Pair(1, 1);
-        List<Pair<Integer, Integer>> orderedPath;
+        Pair p = new Pair<Integer,Integer>(1, 1);
+        List<Pair<Integer, Integer>> orderedPath = new LinkedList<Pair<Integer, Integer>>();
         orderedPath.add(p);
-        PathPosition Pp = new PathPosition(currentPositionInPath, orderedPath);
+        PathPosition Pp = new PathPosition(0, orderedPath);
         BasicEnemy e = new BasicEnemy(Pp, 4, 3, 1, 2, "Jeffery");
         
         assertEquals(e.getSupportRange(), 2);
@@ -62,13 +66,85 @@ public class BasicEnemyTest {
 
     @Test
     public void enemyTest5(){
-        Pair p = new Pair(1, 1);
-        List<Pair<Integer, Integer>> orderedPath;
+        Pair p = new Pair<Integer,Integer>(1, 1);
+        List<Pair<Integer, Integer>> orderedPath = new LinkedList<Pair<Integer, Integer>>();
         orderedPath.add(p);
-        PathPosition Pp = new PathPosition(currentPositionInPath, orderedPath);
+        PathPosition Pp = new PathPosition(0, orderedPath);
         BasicEnemy e = new BasicEnemy(Pp, 4, 3, 1, 2, "Jeffery");
         assertEquals(e.getName(), "Jeffery");
         e.setName("Steve");
         assertEquals(e.getName(), "Steve");
+    }
+
+    @Test
+    public void slugTest(){
+        Pair p = new Pair<Integer,Integer>(1, 1);
+        List<Pair<Integer, Integer>> orderedPath = new LinkedList<Pair<Integer, Integer>>();
+        orderedPath.add(p);
+        PathPosition Pp = new PathPosition(0, orderedPath);
+        Slug e = new Slug(Pp);
+        assertEquals(e.getName(), "Slug");
+        e.setName("Steve");
+        assertEquals(e.getName(), "Steve");
+    }
+
+    @Test
+    public void slugGoldTest(){
+        Pair p = new Pair<Integer,Integer>(1, 1);
+        List<Pair<Integer, Integer>> orderedPath = new LinkedList<Pair<Integer, Integer>>();
+        orderedPath.add(p);
+        PathPosition Pp = new PathPosition(0, orderedPath);
+        Slug e = new Slug(Pp);
+        assertEquals(e.GoldLeft(), 1);
+    }
+
+    @Test
+    public void slugExpTest(){
+        Pair p = new Pair<Integer,Integer>(1, 1);
+        List<Pair<Integer, Integer>> orderedPath = new LinkedList<Pair<Integer, Integer>>();
+        orderedPath.add(p);
+        PathPosition Pp = new PathPosition(0, orderedPath);
+        Slug e = new Slug(Pp);
+        assertEquals(e.ExpLeft(), 2);
+    }
+
+    @Test
+    public void zombieGoldTest(){
+        Pair p = new Pair<Integer,Integer>(1, 1);
+        List<Pair<Integer, Integer>> orderedPath = new LinkedList<Pair<Integer, Integer>>();
+        orderedPath.add(p);
+        PathPosition Pp = new PathPosition(0, orderedPath);
+        Zombie e = new Zombie(Pp);
+        assertEquals(e.GoldLeft(), 3);
+    }
+
+    @Test
+    public void zombieExpTest(){
+        Pair p = new Pair<Integer,Integer>(1, 1);
+        List<Pair<Integer, Integer>> orderedPath = new LinkedList<Pair<Integer, Integer>>();
+        orderedPath.add(p);
+        PathPosition Pp = new PathPosition(0, orderedPath);
+        Zombie e = new Zombie(Pp);
+        assertEquals(e.ExpLeft(), 5);
+    }
+
+    @Test
+    public void vampireGoldTest(){
+        Pair p = new Pair<Integer,Integer>(1, 1);
+        List<Pair<Integer, Integer>> orderedPath = new LinkedList<Pair<Integer, Integer>>();
+        orderedPath.add(p);
+        PathPosition Pp = new PathPosition(0, orderedPath);
+        Vampire e = new Vampire(Pp);
+        assertEquals(e.GoldLeft(), 5);
+    }
+
+    @Test
+    public void vampireExpTest(){
+        Pair p = new Pair<Integer,Integer>(1, 1);
+        List<Pair<Integer, Integer>> orderedPath = new LinkedList<Pair<Integer, Integer>>();
+        orderedPath.add(p);
+        PathPosition Pp = new PathPosition(0, orderedPath);
+        Vampire e = new Vampire(Pp);
+        assertEquals(e.ExpLeft(), 8);
     }
 }
