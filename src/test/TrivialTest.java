7a8d8998318413711b106a0c71774c22652ef055
Yuhan Yan <z5270656@ad.unsw.edu.au>
2021-07-15 14:08:40 +1000

    add tests for cards seesion

--- src/test/TrivialTest.java
+++ src/test/TrivialTest.java
@@ -11,8 +11,8 @@ import unsw.loopmania.LoopManiaWorld;
 
 /**
  * this class is a dummy class demonstrating how to setup tests for the project
- * you should setup additional test classes in a similar fashion, aiming to achieve high coverage.
- * A clickable "Run Test" link should appear if you have installed the Java Extension Pack properly.
+ * you should setup additional test classes in a similar fashion, aiming to achieve high coverage
+ * A clickable "Run Test" link should appear if you have installed the Java Extension Pack properly
  */
 public class TrivialTest {
     @Test
@@ -26,3 +26,4 @@ public class TrivialTest {
         assertEquals(d.getWidth(), 1);
     }
 }
+
