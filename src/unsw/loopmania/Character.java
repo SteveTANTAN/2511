21abd4ad2d89267dc7970293f477db9b177758cb
Anbang Cheng <z5237233@ad.unsw.edu.au>
2021-07-16 11:39:58 +1000

    implement character.java

--- src/unsw/loopmania/Character.java
+++ src/unsw/loopmania/Character.java
@@ -5,8 +5,53 @@ package unsw.loopmania;
  */
 public class Character extends MovingEntity {
     // TODO = potentially implement relationships between this class and other classes
+    private int health;
+    private int aggressivity;
+    private int defense;
+    private List<AlliedSoldier> soldiers;
+    private Item weapon;
     public Character(PathPosition position) {
         super(position);
     }
+
+    public int getHealth() {
+        return this.health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getAggressivity() {
+        return this.aggressivity;
+    }
+
+    public void setAggressivity(int aggressivity) {
+        this.aggressivity = aggressivity;
+    }
+
+    public int getDefense() {
+        return this.defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
     
+    public void addSoldier(AlliedSoldier s) {
+        soldiers.add(s);
+    }
+
+    public List<AlliedSoldier> getSoldiers() {
+        return soldiers;
+    }
+
+    public Item getWeapon() {
+        return this.weapon;
+    }
+
+    public void setWeapon(Equipment weapon) {
+        this.weapon = weapon;
+    }
+
 }
